d9fb69bf611f26f1f1e6631c0969c8335bd2436e
leo <9288062+LeoKavanagh@users.noreply.github.com>
2022-05-24 22:43:22 +0100

    Include no autoresize in image upload option

--- main.js
+++ main.js
@@ -2,28 +2,61 @@ var IMAGE_SRC = "imageSrc"
 
 // image upload
 let fileInput = document.getElementById('fileInput');
+
 fileInput.addEventListener('change', function(ev) { 
-  console.log("detected a change")
-  if(ev.target.files) {
-    let file = ev.target.files[0];
-    var reader  = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = function (e) {
-      var image = new Image();
-      image.src = e.target.result;
-      image.onload = function(ev) {
-        var canvas = document.getElementById(IMAGE_SRC);
-        canvas.width = image.width;
-        canvas.height = image.height;
-        var ctx = canvas.getContext('2d');
-        ctx.drawImage(image,0,0);
+
+    console.log("detected a change")
+
+    if(ev.target.files) {
+
+        console.log('ev.target.files')
+        let file = ev.target.files[0];
+        var reader  = new FileReader();
+
+        reader.readAsDataURL(file);
+        console.log('readAsDataURL')
+
+        reader.onloadend = function (e) {
+
+            var image = new Image();
+            image.src = e.target.result;
+
+            image.onload = function(ev) {
+        
+                var canvas = document.getElementById(IMAGE_SRC);
+                var ctx = canvas.getContext('2d');
+                // canvas.width = image.width;
+                // canvas.height = image.height;
+                // ctx.drawImage(image,0,0);
+
+                console.log(' uploaded image autoresize === false')
+                //clear canvas
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                // set size proportional to image
+                let uploadedRatio = image.height / image.width;
+                canvas.height = canvas.width * (uploadedRatio);
+
+                // step 1 - resize to X%
+                let prop = 1.0 // 0.5
+                var oc = document.createElement('canvas'),
+                    octx = oc.getContext('2d');
+                oc.width = image.width * prop;
+                oc.height = image.height * prop;
+                octx.drawImage(image, 0, 0, oc.width, oc.height);
+
+                // step 2
+                octx.drawImage(oc, 0, 0, oc.width * prop, oc.height * prop);
+
+                // step 3, resize to final size
+                ctx.drawImage(oc, 0, 0, oc.width * prop, oc.height * prop,
+                0, 0, canvas.width, canvas.height);
       }
     }
+
 }});
 
 
-// imagine pasting
-var CLIPBOARD = new CLIPBOARD_CLASS(IMAGE_SRC, false);
 /**
  * image pasting into canvas
  *
@@ -61,22 +94,27 @@ function CLIPBOARD_CLASS(canvas_id, autoresize) {
             }
         }
     };
+
     //draw pasted image to canvas
     this.paste_createImage = function (source) {
         var pastedImage = new Image();
         pastedImage.onload = function () {
-            if(autoresize == true){
+            if(autoresize){
+                console.log('autoresize === true')
                 //resize
                 canvas.width = pastedImage.width;
                 canvas.height = pastedImage.height;
                 ctx.drawImage(pastedImage, 0, 0);
             }
             else{
+                console.log('autoresize === false')
                 //clear canvas
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
                 // set size proportional to image
-                canvas.height = canvas.width * (pastedImage.height / pastedImage.width);
+                let pastedRatio = pastedImage.height / pastedImage.width;
+                canvas.height = canvas.width * (pastedRatio);
+
                 // step 1 - resize to X%
                 let prop = 1.0 // 0.5
                 var oc = document.createElement('canvas'),
@@ -84,8 +122,10 @@ function CLIPBOARD_CLASS(canvas_id, autoresize) {
                 oc.width = pastedImage.width * prop;
                 oc.height = pastedImage.height * prop;
                 octx.drawImage(pastedImage, 0, 0, oc.width, oc.height);
+
                 // step 2
                 octx.drawImage(oc, 0, 0, oc.width * prop, oc.height * prop);
+
                 // step 3, resize to final size
                 ctx.drawImage(oc, 0, 0, oc.width * prop, oc.height * prop,
                 0, 0, canvas.width, canvas.height);
@@ -96,6 +136,8 @@ function CLIPBOARD_CLASS(canvas_id, autoresize) {
     };
 };
 
+// imagine pasting
+var CLIPBOARD = new CLIPBOARD_CLASS(IMAGE_SRC, false);
 
 // Canny edge detection
 function lines(thresh1, thresh2) {
